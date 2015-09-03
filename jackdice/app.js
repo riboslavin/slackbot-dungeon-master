98694a7d7c41f9d5ad8fb7a415708c720af46593
John Peden <john.peden@utexas.edu>
2015-09-03 00:41:27 -0500

    saving work

--- jackdice/app.js
+++ jackdice/app.js
@@ -5,9 +5,6 @@ var hellobot = require('./hellobot');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// hello test
-app.post('/hello', hellobot);
-
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,3 +20,6 @@ app.use(function (err, req, res, next) {
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
 });
+
+// hello test
+app.post('/hello', hellobot);
