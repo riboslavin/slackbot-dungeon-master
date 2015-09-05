0fa9c39a64ed1d7c8e70b5e6243ea3c2eb017c96
John Peden <john.peden@utexas.edu>
2015-09-05 10:09:26 -0500

    added postgres module and connection in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
 var dicebot = require('./dicebot');
+var pg = require('pg');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -20,6 +21,18 @@ app.use(function (err, req, res, next) {
 
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
+});
+
+// connect to database
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
 });
 
 // hello bot
