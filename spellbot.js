c47656be76294efcaed8cb4fd9db6de7ffa09e45
John Peden <john.peden@utexas.edu>
2015-09-11 22:59:08 -0500

    fixing keys with spaces

--- spellbot.js
+++ spellbot.js
@@ -51,7 +51,7 @@ module.exports = function (req, res, next) {
   function characterOutput(character) {
     output = "";
     for (var level in obj.spell_list[character]) {
-      output += "*" + level + ":*\n";
+      output += "*Level " + level + ":*\n";
       for (var spell in obj.spellbook[character][level]) {
         output += spell + "\n";
       }
